87308c0bd7c33a432d735160100b6a053eee0808
Tatiana Malysheva <132544906+Edson20044@users.noreply.github.com>
2023-05-06 10:45:55 +0300

    Update 5. Priority Products_Restocking.sql

--- 5. Priority Products_Restocking.sql
+++ 5. Priority Products_Restocking.sql
@@ -3,20 +3,20 @@ WITH
 
 low_stock_products as (
 	SELECT productCode, 
-		   round(sum(quantityOrdered)*1.0/
+	       round(sum(quantityOrdered)*1.0/
 		    (SELECT quantityInStock
 		       FROM products p
-			  WHERE od.productCode = p.productCode), 2) as low_stock
+		      WHERE od.productCode = p.productCode), 2) as low_stock
 	 FROM orderdetails od
-    GROUP by productCode
-    ORDER by low_stock DESC
-    LIMIT 10
+        GROUP by productCode
+        ORDER by low_stock DESC
+        LIMIT 10
 )
 SELECT productCode, 
-	   sum(quantityOrdered*priceEach) as product_performance
+       sum(quantityOrdered*priceEach) as product_performance
   FROM orderdetails od
  WHERE productCode in (SELECT productCode
-						 FROM low_stock_products)
+			 FROM low_stock_products)
  GROUP by productCode
  ORDER by product_performance DESC
  LIMIT 10; 	
